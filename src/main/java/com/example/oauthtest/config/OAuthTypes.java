90aec5ee0da8f5ccdf7b270ad2d7bc21c31729e5
Close Su <close.su@kyperdata.com>
2018-03-14 21:37:59 +0800

    first ver

---
+++ src/main/java/com/example/oauthtest/config/OAuthTypes.java
@@ -0,0 +1,5 @@
+package com.example.oauthtest.config;
+
+public class OAuthTypes {
+    public static final String GITHUB = "github";
+}
---
+++ src/main/java/com/example/oauthtest/oauth/api/GithubApi.java
@@ -0,0 +1,34 @@
+package com.example.oauthtest.oauth.api;
+
+import org.scribe.builder.api.DefaultApi20;
+import org.scribe.model.OAuthConfig;
+import org.scribe.utils.OAuthEncoder;
+
+public class GithubApi extends DefaultApi20 {
+
+    private static final String AUTHORIZE_URL = "https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s";
+    private static final String SCOPED_AUTHORIZE_URL = AUTHORIZE_URL + "&scope=%s";
+    private static final String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token?";
+
+//    private final String githubState = "";
+
+//    public GithubApi(String state){
+//	this.githubState = state;
+//    }
+
+    @Override
+    public String getAuthorizationUrl(OAuthConfig config) {
+	if (config.hasScope()){
+	    return String.format(SCOPED_AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()),
+		    OAuthEncoder.encode(config.getScope()));
+	}
+	else{
+	    return String.format(AUTHORIZE_URL, config.getApiKey(), OAuthEncoder.encode(config.getCallback()));
+	}
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+	return String.format(ACCESS_TOKEN_URL);
+    }
+}
