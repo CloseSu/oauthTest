1d7443dd3ba12f10872c2e7f2943ac91fbc30502
Close Su <close.su@kyperdata.com>
2018-03-15 11:55:57 +0800

    remove other json parser

--- src/main/java/com/example/oauthtest/oauth/service/GithubOAuthService.java
+++ src/main/java/com/example/oauthtest/oauth/service/GithubOAuthService.java
@@ -1,7 +1,5 @@
 package com.example.oauthtest.oauth.service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONPath;
 import com.example.oauthtest.config.OAuthTypes;
 import com.example.oauthtest.model.OAuthUser;
 import com.example.oauthtest.model.User;
@@ -11,6 +9,10 @@ import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
+
+import java.util.Map;
 
 public class GithubOAuthService extends OAuthServiceDeractor{
 
@@ -28,10 +30,11 @@ public class GithubOAuthService extends OAuthServiceDeractor{
 	Response response = request.send();
 	OAuthUser oAuthUser = new OAuthUser();
 	oAuthUser.setoAuthType(getoAuthType());
-	Object result = JSON.parse(response.getBody());
-	oAuthUser.setoAuthId(JSONPath.eval(result, "$.id").toString());
+	JsonParser jsonParser = JsonParserFactory.getJsonParser();
+	Map<String, Object> result = jsonParser.parseMap(response.getBody());
+	oAuthUser.setoAuthId(String.valueOf(result.get("id")));
 	oAuthUser.setUser(new User());
-	oAuthUser.getUser().setUsername(JSONPath.eval(result, "$.login").toString());
+	oAuthUser.getUser().setUsername((String) result.get("login"));
 	return oAuthUser;
     }
 }
