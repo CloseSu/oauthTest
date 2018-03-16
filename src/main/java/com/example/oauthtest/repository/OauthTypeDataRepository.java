8a281f2c708fb8791a0752406c1a90aae537fe74
Close Su <close.su@kyperdata.com>
2018-03-16 13:39:12 +0800

    remove unuse method

--- src/main/java/com/example/oauthtest/repository/OauthTypeDataRepository.java
+++ src/main/java/com/example/oauthtest/repository/OauthTypeDataRepository.java
@@ -3,10 +3,7 @@ package com.example.oauthtest.repository;
 import com.example.oauthtest.model.OauthTypeData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface OauthTypeDataRepository extends JpaRepository<OauthTypeData, Integer> {
     OauthTypeData findByUsername(String username);
     OauthTypeData findByOAuthType(String oAuthType);
-    List<OauthTypeData> findByUserId(Integer userId);
 }
